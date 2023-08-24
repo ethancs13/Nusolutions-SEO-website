bea9fd10a96faac54f6f8fba39e7e02b151eff35
BuildTools <unconfigured@null.spigotmc.org>
2023-08-23 18:09:49 -0600

    changes

---
+++ Assets/js/script.js
@@ -0,0 +1,72 @@
+const hamMenu = document.querySelector('#hamburger-Menu');
+const dropDown = document.querySelector('#drop-Down');
+
+var check = 0;
+
+function drop(){
+    // dropDown.style.visibility = "visible";
+    // dropDown.style.display = "flex";
+    // dropDown.style.flexDirection = "column";
+    if (check === 0) {
+        var style = document.createElement('style');
+
+        hamMenu.innerHTML = "back";
+        style.innerHTML = `
+            .hamburger-Menu {
+                visiblity: visible;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                height: 40px;
+                color: white;
+                font-weight: bolder;
+            }
+            .hamburger-Menu img {
+                position: cover;
+            }
+            .drop-Down {
+                visibility: visible;
+                display: flex;
+                flex-direction: column;
+                position: absolute;
+                margin-left: 82%;
+                top: 60px;
+                right: 20px;
+            }
+            .drop-Down li a{
+                display: flex;
+                background-color: #2589bd;
+                border: 1px solid white;
+                padding: 5px;
+                margin: 2px;
+                width: 300px;
+                justify-content: center;
+                align-items: center;
+                font-size: 18px;
+            }
+        `;
+
+         document.head.appendChild(style);
+         check = check + 1;
+    } else if (check === 1) {
+        var style = document.createElement('style');
+
+        hamMenu.innerHTML = "";
+        style.innerHTML = `
+            .hamburger-Menu {
+                width: 50px;
+                height: 50px;
+                border: 2px solid white;
+                border-radius: 20px;
+            }
+            .drop-Down {
+                display: none;
+            }
+        `;
+
+        document.head.appendChild(style);
+        check = check - 1;
+    }
+}
+
+hamMenu.addEventListener("click", drop)
---
+++ README.md
@@ -0,0 +1,12 @@
+# Challenge One
+
+## SEO website alterations
+
+* **modified CSS**
+consolidated code into more concise and optimized structure
+
+* **modified HTML**
+added id and class names to some important elements
+
+* **Commented**
+commented in both files for clarity
